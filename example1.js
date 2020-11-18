8d9e22ef7ef6bd72dd5f480fe3a5dbe0c79ae1d9
el7 <bergmandkevin@gmail.com>
2020-11-17 22:19:08 -0800

    new adds

--- example1.js
+++ example1.js
@@ -1,6 +1,14 @@
 
 
 
+
+function myFunc() {
+	var str = "grapes -->";
+	document.getElementById('demo').innerHTML = "azx">
+	alert("test")
+}
+
+
 let examplePoint = new statPointValues(10, 23);
 let examplePoint = new statPointValues(15, 20);
 let examplePoint = new statPointValues(33, 25);

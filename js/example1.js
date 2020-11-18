b8da57baef6d950f88694db16e193eb1d06fdf68
el7 <bergmandkevin@gmail.com>
2020-11-18 00:30:50 -0800

    new adds

---
+++ js/example1.js
@@ -0,0 +1,16 @@
+
+//alert("yello")
+
+console.log("hello");
+
+function funstuff() {
+
+	var strAlertMessage = "grapes -->";
+	document.getElementById('demo').innerHTML = "azx"
+//	alert(strAlertMessage)
+}
+
+
+let examplePoint = new statPoint(23);
+console.log(examplePoint.getHeight());
+
---
+++ js/statPoint.js
@@ -0,0 +1,27 @@
+
+//alert("ok")
+
+
+class statPoint {
+	constructor(intHeightValue) {
+		var intHeight = intHeight;
+	}
+
+	getHeight() {
+		return this.intHeight;
+	}
+
+	setHeight(intHeight) {
+		this.intHeigh = intHeight;
+	}
+
+
+}
+
+
+class statRelationship {
+	//statPointV
+	//statPointV
+}
+
+

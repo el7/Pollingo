8dc1e31fb4f0400ef254cc858ad2af644540148c
el7 <got2skatetoday@gmail.com>
2020-12-03 23:03:34 -0800

    adding header stuff

--- pollingo/src/js/Header.js
+++ pollingo/src/js/Header.js
@@ -4,18 +4,13 @@ import "../css/Header.css";
 function Header() {
     return (
         <div className="header">
-
             <header>
                 <h2>Pollingo.</h2>
                 <h5>tell me something</h5>      
             </header>
-            <main>
-                M
-            </main>
             <aside>
                 User
             </aside>
-
         </div>
     )
 }

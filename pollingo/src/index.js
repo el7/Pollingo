1695508ed65a2040931c8df7cf52c7879ad56ff9
el7 <got2skatetoday@gmail.com>
2020-12-03 22:00:13 -0800

    restructure

--- pollingo/src/index.js
+++ pollingo/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './js/App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './js/reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>

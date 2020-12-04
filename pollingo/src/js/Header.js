2e96110f8c7b0cb6a970905c27bd4e9caff52f52
el7 <got2skatetoday@gmail.com>
2020-12-03 22:48:36 -0800

    adding some framing and structure

--- pollingo/src/js/Header.js
+++ pollingo/src/js/Header.js
@@ -4,8 +4,18 @@ import "../css/Header.css";
 function Header() {
     return (
         <div className="header">
-            <h2> Page Header.</h2>
-            <h5>tell me something</h5>      
+
+            <header>
+                <h2>Pollingo.</h2>
+                <h5>tell me something</h5>      
+            </header>
+            <main>
+                M
+            </main>
+            <aside>
+                User
+            </aside>
+
         </div>
     )
 }

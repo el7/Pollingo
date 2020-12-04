f686632536a0e6dff504e4260eacfedbb9937b58
el7 <got2skatetoday@gmail.com>
2020-12-04 01:38:32 -0800

    formatting some tabs. sleep time.

--- pollingo/src/js/PollBox.js
+++ pollingo/src/js/PollBox.js
@@ -11,10 +11,23 @@ function PollBox() {
                 ASIDE. Polling types go here. 
             </aside>
             <footer>
-                FOOTER. Selections go here.
+                <div class="tabContainer"> 
+                    <div class="buttonContainer"> 
+                        <button onclick="showPanel(0,'#234456')">IRV</button>
+                        <button onclick="showPanel(1,'#745346')">Approval</button>
+                        <button onclick="showPanel(2,'#d34565')">RC</button>
+                        <button onclick="showPanel(3,'#f45546')">STAR</button>                                                
+                    </div>
+                    <div class="tabPanel">Content</div>
+                    <div class="tabPanel">Content</div>
+                    <div class="tabPanel">Content</div>
+                    <div class="tabPanel">Content</div>
+                </div>                                                
             </footer>
         </div>
     )
 }
+
+<script src="./PollBox_tabs.js"></script>
 
 export default PollBox

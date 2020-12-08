7918ca6a49bb52777a7a5c2092c4ba54ff180786
el7 <got2skatetoday@gmail.com>
2020-12-08 14:55:09 -0800

    adding in some mongo atlas db

--- pollingo/src/js/App.js
+++ pollingo/src/js/App.js
@@ -1,3 +1,11 @@
+//const express = require("express");
+//const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect(`mongodb+srv://dbBergmank:${process.env.MONGO_ATLAS_PW}@cluster0.ssk8u.mongodb.net/<dbname>?retryWrites=true&w=majority`);
+
 import React from "react";
 import '../css/main.css';
 import Header from './Header';

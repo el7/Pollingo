a9a48f87f5818b8940a6501ce4891e66a45ef31f
el7 <got2skatetoday@gmail.com>
2020-12-23 20:49:14 -0800

    abstracted Q's and A's into vairables. going to try generating them some way.

--- pollingo/src/js/components/layoutPollingoOne.js
+++ pollingo/src/js/components/layoutPollingoOne.js
@@ -7,9 +7,13 @@ import "../../css/main.css";
 
 function LayoutOne() {
 
-
-
-
+    // function that generates polling questions
+    
+    const q1 = "Which fast food would you eat forever?";
+    const q1_a1 = "Taco Bell";
+    const q1_a2 = "Chic Fillet";
+    const q1_a3 = "In-N-Out";
+    const q1_a4 = "Panda Express";
 
     return (
         <div className="layoutOne grid-main-container">
@@ -17,7 +21,7 @@ function LayoutOne() {
                 <Header/>
             </div>
             <div className="grid-item grid-main-item-main">
-                <PollBox q="Which fast food would you eat forever?" a1="Taco Bell" a2="Chic Fillet" a3="In-N-Out" a4="Panda Express"/>
+                <PollBox q={q1} a1={q1_a1} a2={q1_a2} a3={q1_a3} a4={q1_a4}/>
                 <Seperator />
                 <PollBox q="Which is your favorite vehicle brand?" a1="Tesla" a2="Subaru" a3="Ford" a4="Volkswagon"/>
                 <Seperator />                

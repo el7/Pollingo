240a96e37e025d1a9d921b06896bfb2c1dd490ff
el7 <got2skatetoday@gmail.com>
2020-12-25 20:34:47 -0800

    fixing header to top

---
+++ pollingo/src/js/components/separatorComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "../../css/main.css";
+
+function Separator(props) {
+    return (
+        <div className="separator-1-div">
+            <span className="separator-1-span">{props.t}</span>
+        </div>
+    )
+}
+
+export default Separator;
+

9cca6c8a52e95788f56c56ef74fb7c9eb8b383a2
el7 <got2skatetoday@gmail.com>
2021-04-23 19:40:30 -0700

    adding results component. will eventually show as graph of data

---
+++ src/js/components/resultsComponent.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+import ReactDOM from 'react-dom';
+import "../../css/main.css";
+
+
+function Results() {
+
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default Results;

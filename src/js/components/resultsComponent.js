e0f287d6fe76fe72eefed30b5d1a46ce68373ad5
el7 <got2skatetoday@gmail.com>
2021-04-30 20:46:55 -0700

    adding the graph containers.

--- src/js/components/resultsComponent.js
+++ src/js/components/resultsComponent.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import "../../css/main.css";
 
 function ResultBox() {
+
+    /* Need code to get poll data from Mongodb 
+        - question
+        - selection text 1, 2, 3, 4
+        - selection values 1, 2, 3, 4
+    */
+
+
+
     return (
         <div className="resultbox-container">
-        <div className="resultbox-graph-container">
-            <p>This is where the results go for the currenly selected poll. </p>
-        </div>
+            <div className="resultbox-graph-container">
+
+                <p>~~~~~<br></br>This is where the results go for the currenly selected poll. </p>
+            </div>
             <p>This is the outer-area, not sure what will go here.</p>
         </div>
     )

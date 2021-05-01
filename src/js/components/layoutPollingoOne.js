09a611a97ba70bf6b87b5838b2903a8d8e64a60d
el7 <got2skatetoday@gmail.com>
2021-04-30 18:38:10 -0700

    simplifying the CSS until I get more functionality. too many CSS rules causing problems at larger screens.

--- src/js/components/layoutPollingoOne.js
+++ src/js/components/layoutPollingoOne.js
@@ -1,7 +1,6 @@
 // React components
-import React, {useState} from 'react';
+import React from 'react';
 import Header from './headerComponent';
-import HeaderLower from './headerLowerComponent';
 import PollBox from './pollboxComponent';
 import Console from './consoleComponent';
 import Separator from './separatorComponent';
@@ -10,38 +9,26 @@ import ResultBox from './resultsComponent';
 // CSS
 import "../../css/main.css";
 
-
 function LayoutOne() {
 
-    const [question1, setQuestion1setScreenWidth] = useState("200");
-
     // function that generates polling questions    
-    const q1 = "Which fast food would you eat forever?";
-    const q1_a1 = "Taco Bell";
-    const q1_a2 = "Chic Fillet";
-    const q1_a3 = "In-N-Out";
-    const q1_a4 = "Panda Express";
+    const q1 = "What is the best movie ever?";
+    const q1_a1 = "The Matrix";
+    const q1_a2 = "V For Vendetta";
+    const q1_a3 = "The Big Lebowski";
+    const q1_a4 = "Tron";
 
-    const t1 = "sports";
-    const t2 = "politics";
-    const t3 = "food";
+    const separator_text = "➽─────❥";
 
     return (
         <div className="layoutOne grid-main-container">
             <div className="grid-item grid-main-item-header">
                 <Header/>
-                {/* <HeaderLower tag1={t1} tag2={t2} tag3={t3}/> */}
             </div>
             <div className="grid-item grid-main-item-main grid-main">
                 <ResultBox />
                 <PollBox q={q1} a1={q1_a1} a2={q1_a2} a3={q1_a3} a4={q1_a4}/>
-                <Separator t="➽─────❥"/>
-                {/*
-                <PollBox q="Which is your favorite vehicle brand?" a1="Tesla" a2="Subaru" a3="Ford" a4="Volkswagon"/>
-                <Separator t="➽─────❥"/>
-                <PollBox q="What is your favorite movie?" a1="Terminator 2: Judgement Day" a2="Star Wars: Episode V - The Empire Strikes Back" a3="Oh Brother, Where Art Thou" a4="The Big Lebowski"/>                
-                <Separator t="➽─────❥"/>
-                */}
+                <Separator t={separator_text}/>
             </div>
             <div className="grid-item grid-main-item-footer">
                 <Console />

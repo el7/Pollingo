af94980d655a4e9af8b93279b27f40d1983c9839
el7 <got2skatetoday@gmail.com>
2021-04-30 20:22:14 -0700

    adding the graph containers.

--- src/js/components/resultsComponent.js
+++ src/js/components/resultsComponent.js
@@ -2,13 +2,13 @@ import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import "../../css/main.css";
 
-
 function ResultBox() {
     return (
-        <div className="resultbox-graph">
+        <div className="resultbox-container">
+        <div className="resultbox-graph-container">
             <p>This is where the results go for the currenly selected poll. </p>
             <p>React will change graph based on current state data.</p>
-        </div>
+        </div></div>
     )
 }
 

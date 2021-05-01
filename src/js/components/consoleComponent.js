780a7b8a039d713049f4381e303c0e5ca18b6dd8
el7 <got2skatetoday@gmail.com>
2021-04-30 17:19:23 -0700

    simplifying the CSS until I get more functionality. too many CSS rules causing problems at larger screens.

--- src/js/components/consoleComponent.js
+++ src/js/components/consoleComponent.js
@@ -5,12 +5,10 @@ import "../../css/main.css";
 function Console() {
     return (
         <nav className="console">
-            <h2 className="console-name">CONSOLE SHT</h2>
-            <p className="console-desc">Welcome to the console yo.</p>
+            <h2 className="console-name">CONSOLE</h2>
             <ToggleRankedVoting />
         </nav>
     )
 }
 
 export default Console
-
